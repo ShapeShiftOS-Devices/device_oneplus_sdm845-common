a2ee98a57023be9de6da8ba921775173be3ebaaf
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-09 16:52:33 +0200

    sdm845-common: Move soong namespaces to common.mk
    * This is a product property
    
    Change-Id: I411428687839fca7c794d488bb48e5f1773e570b

--- common.mk
+++ common.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     RemovePackages
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
