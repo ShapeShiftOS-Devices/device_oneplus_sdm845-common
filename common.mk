eaf5ca51395c0e29d5f35f7169b9f014b87dc330
maxwen <max.weninger@gmail.com>
2020-02-05 23:12:51 +0100

    sdm845-common: Use EmptyOverlay to bind mount oneplus icons
    Using /dev/null breaks idmap2 run
    which also breaks valid static overlays that might be in /product/overlay
    
    [ anirudhgupta109 | AOSiP ]
     - This also reverts commit a044e64fcde10be778a6f4583510dd1d9f1c1ed3.
     - Change paths to /system/product instead of /system
    
    Change-Id: I6e54bb0781a11ad540fd925c0e7124fd4d658eb4
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>

--- common.mk
+++ common.mk
@@ -25,12 +25,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosp
 
-PRODUCT_PACKAGES += \
-    OnePlusIconShapeCircleOverlay \
-    OnePlusIconShapeRoundedRectOverlay \
-    OnePlusIconShapeSquareOverlay \
-    OnePlusIconShapeSquircleOverlay \
-    OnePlusIconShapeTeardropOverlay
 
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
