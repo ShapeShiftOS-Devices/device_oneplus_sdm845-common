67d6f3e6e51a0b7fe16cfff9074dfbb285081d1d
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-01-16 19:49:11 +0530

    sdm845-common: vendorsetup: setup HALs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,4 +10,8 @@ sudo git clone https://github.com/ShapeShiftOS/android_hardware_qcom_audio -b an
 sudo git clone https://github.com/ShapeShiftOS/android_hardware_qcom_media -b android_11-caf-sdm845 hardware/qcom-caf/sdm845/media
 sudo git clone https://github.com/ShapeShiftOS/android_hardware_qcom_display -b android_11-caf-sdm845 hardware/qcom-caf/sdm845/display
 
-
+echo "modifiying HALs for ShapeShift"
+cd hardware/qcom-caf/sdm845/display
+sudo git reset --hard 66f4fe780cddc7361d7ad1490ed4c01ad23cd856
+cd ../../../..
+sudo rm -rf vendor/qcom/opensource/commonsys-intf/display
